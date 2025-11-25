5846047546f6c28d4df78019b12ef19a5bc89b98
Johnny Dreher <johnnydreher@gmail.com>
2025-11-25 17:35:07 -0300

    feat: Update VisionConstants and remove camera parameters
    - Removes unused camera configuration constants (`CAMERA_HEIGHT_METERS`, `TARGET_HEIGHT_METERS`, `CAMERA_PITCH_DEGREES`, `UPDATE_POSE_VISION_TIMEOUT`).
    - Updates `LONGEST_RPM` from 4600 to 4350.
    
    Signed-off-by: Johnny Dreher <johnnydreher@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionConstants.java
@@ -23,15 +23,9 @@ public class VisionConstants {
     /** The feedforward gain for the turn controller. */
     public static double TURN_KF = 0.1;
 
-    /** The height of the camera from the ground in meters. */
-    public static double CAMERA_HEIGHT_METERS = 0.24554;
-    /** The height of the target from the ground in meters. */
-    public static double TARGET_HEIGHT_METERS = 0.72;
-    /** The pitch of the camera in degrees. */
-    public static double CAMERA_PITCH_DEGREES = 24.44;
-    public static double UPDATE_POSE_VISION_TIMEOUT = 2000;
+
     public static double LONGEST_HOOD = 0.7;
-    public static double LONGEST_RPM = 4600;
+    public static double LONGEST_RPM = 4350;
     public static double LONGEST_DISTANCE = 2.2;
     // ---- Tempo ----
     // distância máxima em METROS
